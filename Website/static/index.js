25e3ab851c3895c1548ded91dd52248f32dcb30a
Brogtastic <mbrogdon@student.fitchburgstate.edu>
2023-06-16 16:48:03 -0400

    Search bar added to page, making it function is coming next

--- Website/static/index.js
+++ Website/static/index.js
@@ -1,5 +1,6 @@
+const currentPath = window.location.pathname;
+
 function deleteNote(noteId){
-    const currentPath = window.location.pathname;
 
     fetch('/delete-note', {
         method: 'POST',
@@ -13,46 +14,49 @@ function deleteNote(noteId){
     });
 }
 
+if(currentPath === '/' | currentPath==='/browse'){
+    const cards = document.querySelectorAll('.card');
 
-const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        let isMouseOverCard = false;
 
-cards.forEach(card => {
-    let isMouseOverCard = false;
+        card.addEventListener('mouseover', () => {
+            isMouseOverCard = true;
+            const deleteButton = card.querySelector('.delete-button');
+            if (deleteButton) {
+                deleteButton.style.opacity = 0.3;
+                deleteButton.style.transition = 'opacity 0.2s ease-in-out';
+            }
+        });
 
-    card.addEventListener('mouseover', () => {
-        isMouseOverCard = true;
-        const deleteButton = card.querySelector('.delete-button');
-        if (deleteButton) {
-            deleteButton.style.opacity = 0.3;
-            deleteButton.style.transition = 'opacity 0.2s ease-in-out';
+        card.addEventListener('mouseout', () => {
+            isMouseOverCard = false;
+            const deleteButton = card.querySelector('.delete-button');
+            if (deleteButton) {
+                deleteButton.style.opacity = 0;
+                deleteButton.style.transition = 'opacity 0.2s ease-in-out';
+            }
+        });
+
+
+        if (!isMouseOverCard) {
+            isMouseOverCard = false;
+            const deleteButton = card.querySelector('.delete-button');
+            if (deleteButton) {
+                deleteButton.style.opacity = 0;
+            }
         }
     });
+}
 
-    card.addEventListener('mouseout', () => {
-        isMouseOverCard = false;
-        const deleteButton = card.querySelector('.delete-button');
-        if (deleteButton) {
-            deleteButton.style.opacity = 0;
-            deleteButton.style.transition = 'opacity 0.2s ease-in-out';
-        }
+if(currentPath === '/'){
+    const textarea = document.getElementById('note');
+    const counter = document.getElementById('counter');
+    const maxCount = 1500;
+
+    textarea.addEventListener('input', function() {
+        const remainingChars = maxCount - textarea.value.length;
+        counter.innerHTML = remainingChars;
     });
-
-
-    if (!isMouseOverCard) {
-        isMouseOverCard = false;
-        const deleteButton = card.querySelector('.delete-button');
-        if (deleteButton) {
-            deleteButton.style.opacity = 0;
-        }
-    }
-});
-
-const textarea = document.getElementById('note');
-const counter = document.getElementById('counter');
-const maxCount = 1500;
-
-textarea.addEventListener('input', function() {
-    const remainingChars = maxCount - textarea.value.length;
-    counter.innerHTML = remainingChars;
-});
+}
 
